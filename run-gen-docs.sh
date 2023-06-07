10793418b72f50f96948a2c3307ef0d8a103ff59
Krishanu Singh <krishanu230@hotmail.com>
2023-06-07 14:40:00 +0530

    Update run-gen-docs.sh

--- run-gen-docs.sh
+++ run-gen-docs.sh
@@ -1,97 +1,202 @@
-#!/bin/sh -uex
-echo "Knowl script running to generate ai docs..."
+#!/bin/bash
 
-BIN_PATH="$HOME"
-WORKING_DIR="$BIN_PATH/knowl_temp"
-KNOWL_CODE2DOC_NAME="code2doc.zip"
-CODE2DOC_DOWNLOAD_URL='https://releases.knowl.io/code2doc.zip'
+ANALYZE_NAME="analyze"
+ANALYZE_DOWNLOAD_URL='https://releases.knowl.io/analyze'
+GENERATOR_NAME="generate"
+GENERATOR_DOWNLOAD_URL='https://releases.knowl.io/generator'
+IGNORE_NAME="ignore"
+IGNORE_DOWNLOAD_URL='https://releases.knowl.io/ignore'
+KNOWL_UTILS_NAME="knowl-utils"
+KNOWL_UTILS_DOWNLOAD_URL='https://releases.knowl.io/knowl-utils'
 
+check_envs() {
+  local unset_envs=()
+  for env_var in "$@"; do
+    if [[ -z "${!env_var}" ]]; then
+      unset_envs+=("$env_var")
+    fi
+  done
 
-get_abs_filename() {
-  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+  if [[ ${#unset_envs[@]} -gt 0 ]]; then
+    echo "Error: The following environment variables are unset:"
+    for unset_env in "${unset_envs[@]}"; do
+      echo "  - $unset_env"
+    done
+    exit 1
+  fi
 }
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-if [ -z "$1" ]
-then
-    echo 'no repo path provided'
-    exit 0
-fi
-RESULT_DIR="result"
-RESULT_DIR=$(get_abs_filename ${RESULT_DIR})
-mkdir -p ${RESULT_DIR}
-
-REPO_PATH=${1}
-REPO_PATH=$(get_abs_filename ${REPO_PATH})
+load_env() {
+  if [[ -f "$1" ]]; then
+    set -o allexport
+    source .env
+    set +o allexport
+    echo "Environment variables loaded from $1"
+  fi
+}
 
 verify_wget() {
     BIN_WGET=$(which wget) || {
         echo "You need to install 'wget' to use this hook."
+        exit 1
     }
 }
 
-verify_unzip() {
-    BIN_UNZIP=$(which unzip) || {
-        echo "You need to install 'unzip' to use this hook."
-    }
+get_abs_filename() {
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 }
 
-verify_tmp() {
-    touch $BIN_PATH || {
-        error_out "Could not write to $BIN_PATH"
-    }
-}
+ensure_directory_exists() {
+  local directory_path="$1"
 
-create_working_dir(){
-    working_dir=$1
-    if [ ! -d "$working_dir" ]
-        then
-            mkdir -p -- "$working_dir"
-    fi
+  if [ ! -d "$directory_path" ]; then
+    mkdir -p "$directory_path"
+    echo "Directory created: $directory_path"
+  else
+    echo "Directory already exists: $directory_path"
+  fi
 }
-
 
 download_from_link() {
     echo "download begins ..."
     echo "$1"
+    echo "${BIN_WGET}"
     download_url="$1"
     directory_name="$2"
     file_path="$3"
     
-    create_working_dir $directory_name
+    ensure_directory_exists $directory_name
     $BIN_WGET --no-check-certificate $download_url -O $file_path
     chmod +x $file_path
     echo "download ends ..."
-
 }
 
-check_knowl_cli_version() {
-    echo "downloading the latest cli version"
-    file_url=$CODE2DOC_DOWNLOAD_URL
-    #get folder names in the working directory
-    download_from_link $file_url $WORKING_DIR/ $WORKING_DIR/$KNOWL_CODE2DOC_NAME
 
-    export PATH=$PATH:$WORKING_DIR
+directory_path="$1"
+if [ -z "$directory_path" ]; then
+  echo "Directory path is missing."
+  exit 1
+fi
+directory_path=$(get_abs_filename ${directory_path})
+RESULT_DIR="${directory_path}/knowl_results"
+SHELL_LOG_FILE=$RESULT_DIR/shell_log.txt
+touch "$SHELL_LOG_FILE"
+# Save the original stdout to file descriptor 3
+exec 3>&1
+exec > >(tee $SHELL_LOG_FILE)
+#RESULT_DIR=$(get_abs_filename ${RESULT_DIR})
+ANALYSED_RESULT_DIR=$RESULT_DIR/analysed
+ensure_directory_exists ${RESULT_DIR}
+ensure_directory_exists $ANALYSED_RESULT_DIR
 
+verify_python() {
+  if command -v python3.9 &>/dev/null; then
+    echo "Python is installed."
+    python_version=$(python3.9 -V 2>&1)
+    echo "Python version: $python_version"
+  else
+    echo "Python is not installed."
+    exit 1
+  fi
 }
 
-cleanup() {
-    echo "Cleaning up..."
+verify_ts_node() {
+  # Check if ts-node is installed
+  if command -v ts-node &>/dev/null; then
+    echo "ts-node is installed."
+    
+    # Check the ts-node version
+    ts_node_version=$(ts-node --version 2>&1)
+    echo "ts-node version: $ts_node_version"
+  else
+    echo "ts-node is not installed."
+    exit 1
+  fi
 }
 
-#machine_type=""
+#./genEnv.sh
+load_env ".env"
+check_envs "KNOWL_BACKEND_HTTP" "REPO_NAME" "OWNER_NAME" "BRANCH_NAME" "COMMIT_SHA" "OPENAI_API_KEY" "KNOWL_API_KEY"
+#verify_python
+verify_ts_node
 verify_wget
-verify_unzip
-verify_tmp
-check_knowl_cli_version
-cd $WORKING_DIR
-$BIN_UNZIP $WORKING_DIR/$KNOWL_CODE2DOC_NAME -d $WORKING_DIR
-cd s3/code_to_doc
-pip install -r requirements.txt
-python3.9 python_docs.py -p $REPO_PATH -o $RESULT_DIR
-cat $RESULT_DIR/id_to_pages.json
-cd ../knowl-utils
-npm install -g typescript@4.8.4
-npm install --save-dev -g ts-node@10.9.1
-npm install --save node-fetch@2.6.2 @types/node-fetch@2.6.2 dotenv cmd-ts http-status-codes
-ts-node src/index.ts importer $RESULT_DIR
+
+echo ""
+echo "----getting the latest knowl tools----"
+download_from_link $IGNORE_DOWNLOAD_URL ./ ./$IGNORE_NAME
+download_from_link $ANALYZE_DOWNLOAD_URL ./ ./$ANALYZE_NAME
+download_from_link $GENERATOR_DOWNLOAD_URL ./ ./$GENERATOR_NAME
+download_from_link $KNOWL_UTILS_DOWNLOAD_URL ./ ./$KNOWL_UTILS_NAME
+
+echo ""
+echo "----bucketing files----"
+./bucket_files.sh $directory_path $RESULT_DIR/buckets
+exit_code_script1=$?
+
+#step 1: bucket files
+if [ $exit_code_script1 -eq 0 ]; then
+  echo "bucketing files successful."
+else
+  echo "bucketing files failed."
+  exit 1
+fi
+
+echo ""
+echo "----ignoring files----"
+#cd preprocessers/ignore/build
+#npm i
+./ignore ignore $directory_path $RESULT_DIR/buckets
+exit_code2=$?
+if [ $exit_code2 -eq 0 ]; then
+  echo "ignore script executed successfully."
+else
+  echo "ignore script failed with exit code $exit_code."
+  exit 1
+fi
+#cd ../../../
+
+echo ""
+echo "----running analysers----"
+#step2 run analysers
+#python3.9 -m pip install --upgrade pip
+##pip install -r ./analysers/python/requirements.txt
+#python3.9 analysers/python/python_docs.py  -o $RESULT_DIR/analysed -r $RESULT_DIR/buckets/py_files.json
+#exit_code2=$?
+
+#if [ $exit_code2 -eq 0 ]; then
+#  echo "Python analyser executed successfully."
+#else
+##  echo "Python analyser failed with exit code $exit_code."
+#fi
+
+#cd analysers/ts/build
+#npm i
+./analyze analyze $RESULT_DIR/buckets/ts_files.json -o $RESULT_DIR/analysed
+exit_code2=$?
+if [ $exit_code2 -eq 0 ]; then
+  echo "Ts analyser executed successfully."
+else
+  echo "Ts analyser failed with exit code $exit_code."
+fi
+#cd ../../../
+
+echo ""
+echo "----running generator----"
+# Restore stdout to the console
+exec 1>&3
+
+# Close file descriptor 3
+exec 3>&-
+# Restoring stdout to its original state
+
+#step3 run generator
+#cd generator/
+#npm i
+./generate gendoc $RESULT_DIR/analysed $directory_path -f false -o $RESULT_DIR/generated/ -c true
+#ts-node src/index.ts gendoc $RESULT_DIR/analysed $directory_path -f false -o $RESULT_DIR/generated/ -c true
+#./generator/build/generator gendoc $RESULT_DIR/analysed $directory_path -f false -o $RESULT_DIR/generated/ -c true
+
+exec > >(tee -a $SHELL_LOG_FILE)
+echo ""
+echo "----running importer----"
+./knowl-utils importer $RESULT_DIR/generated/ html
