b32d9c807ab6ca9902aa57bdffe1338abbcac363
krishanu230 <krishanu230@hotmail.com>
2023-06-07 16:39:03 +0530

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 ls
 pwd
+echo "---"
+ls /
 # `$#` expands to the number of arguments and `$@` expands to the supplied `args`
 printf '%d args:' "$#"
 printf " '%s'" "$@"
 printf '\n'
+env
