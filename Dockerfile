86db539d6bd3ad18590fb9ff28a797077be6f7d2
krishanu230 <krishanu230@hotmail.com>
2023-06-07 18:41:21 +0530

    .

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 # Container image that runs your code
 FROM public.ecr.aws/t7d0n7r6/knowl-code-to-doc-manual:latest
 COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,14 +11,7 @@ export OWNER_NAME="$owner"
 export KNOWL_BACKEND_HTTP="https://api.knowl.io/"
 export BRANCH_NAME="$GITHUB_REF_NAME"
 export GIT_PLATFORM="github"
-echo "---"
 ls /
-echo "---"
-ls /usr/app/
-# `$#` expands to the number of arguments and `$@` expands to the supplied `args`
-printf '%d args:' "$#"
-printf " '%s'" "$@"
-printf '\n'
-env
+
 cd /usr/app
 ./start_doc_gen.sh $GITHUB_WORKSPACE
